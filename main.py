e4b81683434d0422c03e4b98a214ef99a1633ccb
Esfahan <calito0131@gmail.com>
2021-02-07 13:30:20 +0900

    Fixed

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def callback():
 # Reply the message 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
     line_bot_api.reply_message(
         event.reply_token,
         #TextSendMessage(text=event.message.text))
